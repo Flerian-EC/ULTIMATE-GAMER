defea73777d5d737f447145dac50a97e0f35c101
fabian escalera <fabianjuelescalera@gmail.com>
2022-03-26 19:14:49 -0400

    funcion de header transparente añadida

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -5,15 +5,26 @@ class MyHeader extends HTMLElement {
     this.image = this.getAttribute("url-image");
     this.color = this.getAttribute("button-color");
     this.headerMov = this.getAttribute("header-movement");
+    this.transparent = this.getAttribute("transparent-header");
   }
 
   //Esta funcion contiene el css del componente
   styles() {
     let col;
+    let tra;
+    let positio;
     if(this.color === null) {
       col = "teal";
     }else {
       col = this.color;
+    }
+
+    if(this.transparent !== "true") {
+      tra = "white";
+      positio = "static";
+    }else {
+      tra = "transparent";
+      positio = "absolute";
     }
 
     const css = `
@@ -27,8 +38,11 @@ class MyHeader extends HTMLElement {
         display: block;
         width: 100%;
         height: 70px;
-        background-color: white;
         will-change: transform;
+        position: ${positio};
+        background-color: ${tra};
+        top: 0;
+        left: 0;
       }
       .my-header {
         width: 100%;
@@ -110,6 +124,16 @@ class MyHeader extends HTMLElement {
       butt.classList.toggle("button-animation");
     })
   }
+  //Esta funcion ejecuta la transparencia del header
+  transparetHeader(val) {
+    if(this.transparent === "true" && val === true) {
+      this.style.backgroundColor = "rgba(255,255,255,20%)"
+      this.style.backdropFilter = "blur(6px)"
+    }else if(this.transparent === "true" && val === false) {
+      this.style.backgroundColor = "transparent"
+      this.style.backdropFilter = "unset"
+    }
+  }
   //Esta funcion es la que activa el seguimiento del header en pantalla
   headerMovement() {
     if(this.headerMov !== "false") {
@@ -120,10 +144,15 @@ class MyHeader extends HTMLElement {
 
         if(move != 0) {
           this.style.boxShadow = "rgb(0 0 0 / 50%) 0px -1px 20px 0px";
+          this.transparetHeader(true)
+        }else {
+          this.style.boxShadow = "unset";
+          this.transparetHeader(false)
         }
       })
     }
   }
+
 
   //Esta funcion se activa automaticamente al crear el componente '<my-header></my-header>' en HTML.
   connectedCallback() {
@@ -140,8 +169,10 @@ class MyHeader extends HTMLElement {
   Este es el componente web en HTML:
   <my-header></my-header>
   Estos son los tipos de atributos que tiene el componente web:
-  url-image: Define la ruta de img que buscara el componete
-  button-color: Define el color del boton del componente
-  header-movement: Define si el componente te seguira o no en pantalla
+
+  -url-image="string" Define la ruta de img que buscara el componete;
+  -button-color="colorCss" Define el color del boton del componente;
+  -header-movement="true / false" Define si el componente te seguira o no en pantalla;
+  -transparent-header="true / false" define si el header es transparente o no;
 */
 window.customElements.define("my-header", MyHeader);
